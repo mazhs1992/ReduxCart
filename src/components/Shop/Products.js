541fa75d46d408f7b6905fd452f6996240ccc5df
Vangelis Mazis <mazhs1992@hotmail.com>
2023-04-10 14:01:31 +0300

    Dispatch actions, store for cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,38 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DummyProducts = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first Book',
+    description: 'The first book i ever wrote!'
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My second Book',
+    description: 'The second book i ever wrote!'
+  }
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DummyProducts.map(
+          (product) => (
+            <ProductItem
+              id={product.id}
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          ))
+        }
+
       </ul>
     </section>
   );
